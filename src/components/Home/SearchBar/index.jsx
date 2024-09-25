42ddd84694e3f89b0fa49feaccffa4f76237a8a2
Brijes Kushwaha <brijeshkushwaha386@gmail.com>
2024-09-25 17:45:01 +0530

    Initial commit

---
+++ src/components/Home/SearchBar/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './styles.css';
+// import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
+
+const SearchBar = ({ value, changeInput }) => (
+  <div className='searchBar-wrap'>
+    {/* <SearchIcon /> */}
+    <input
+      type='text'
+      placeholder='Woodland Hills'
+      value={value}
+      onChange={changeInput}
+    />
+  </div>
+);
+
+export default SearchBar;
